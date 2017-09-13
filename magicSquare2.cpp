5f6702327a2e05505f4a1819778b83f7694d1813
Zhangyzh36 <771235191@qq.com>
2017-09-13 21:38:06 +0800

    Add files via upload

--- magicSquare2.cpp
+++ magicSquare2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void generateMagicSquare(int size);
+int width(int size);
 
 int main()
 {
@@ -16,6 +18,7 @@ int main()
 		cin >> size; 
 	}
 	
+	cout << "The magic square of size " << size << ":" << endl;
 	generateMagicSquare(size);
 	
 	return 0;
@@ -31,34 +34,48 @@ void generateMagicSquare(int size)
 			arr[i][j] = 0;
 	}
 	
+	int x = 0;
+	int y = size / 2;
+		
 	for (int i = 1; i <= size * size; ++i )
 	{
-		int x = 0;
-		int y = size / 2;
+		
+		arr[x][y] = i;
 		
 		int diagonalx = (x + size - 1) % size;
-		int diagonaly = (y + 1) & size;
+		int diagonaly = (y + 1) % size;
 		
 		if (arr[diagonalx][diagonaly])
-		{
-			
-		}
+			x = (x + 1) % size;
 		else
 		{
-			
+			x = diagonalx;
+			y = diagonaly;
 		}
-		
 		
 	}
 	
 	for (int i = 0; i < size; ++i)
 	{
 		for (int j = 0; j < size; ++j)
-			cout << arr[i][j]
+			cout << setw(width(size)) << right << arr[i][j];
 		cout << endl;
 	}		
 	
 	for (int i = 0; i < size; ++i)
 		delete []arr[i];
 	delete []arr;
+}
+
+int width(int size)
+{
+	int num = size * size;
+	int count = 0;
+	while (num)
+	{
+		num /= 10;
+		count++;
+	}
+	
+	return ++count;
 }
